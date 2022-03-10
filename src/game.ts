f69d5585686801e925a7329cac161d60050e83e0
Elsa Tom <elsa.tom@happiestminds.com>
2022-03-10 17:29:24 +0530

    modified file path

--- src/game.ts
+++ src/game.ts
@@ -15,7 +15,7 @@ const sceneMessageBus = new MessageBus()
 const entity = new Entity('entity')
 engine.addEntity(entity)
 entity.setParent(_scene)
-const gltfShape = new GLTFShape("78300a73-f19a-4e91-bb8a-08a641e29706/FloorBaseWood_01/FloorBaseWood_01.glb")
+const gltfShape = new GLTFShape("models/FloorBaseWood_01/FloorBaseWood_01.glb")
 gltfShape.withCollisions = true
 gltfShape.isPointerBlocker = true
 gltfShape.visible = true
@@ -69,37 +69,37 @@ const transform9 = new Transform({
   scale: new Vector3(1, 1, 1)
 })
 outdoorTable.addComponentOrReplace(transform9)
-const gltfShape5 = new GLTFShape("ea9146e1-5666-40da-80c9-8a3ce23fb979/Table_01/Table_01.glb")
+const gltfShape5 = new GLTFShape("models/Table_01/Table_01.glb")
 gltfShape5.withCollisions = true
 gltfShape5.isPointerBlocker = true
 gltfShape5.visible = true
 outdoorTable.addComponentOrReplace(gltfShape5)
 
-const armchairRed = new Entity('armchairRed')
-engine.addEntity(armchairRed)
-armchairRed.setParent(_scene)
-const transform10 = new Transform({
-  position: new Vector3(5.5, 0, 18),
-  rotation: new Quaternion(-0.013521425426006317, 0.7716787457466125, 0.013521332293748856, 0.6357250213623047),
-  scale: new Vector3(1, 1, 1)
-})
-armchairRed.addComponentOrReplace(transform10)
-const gltfShape6 = new GLTFShape("dc2d6a5f-0dc7-4300-8015-46993a3c425d/RedArmchair.glb")
-gltfShape6.withCollisions = true
-gltfShape6.isPointerBlocker = true
-gltfShape6.visible = true
-armchairRed.addComponentOrReplace(gltfShape6)
+// const armchairRed = new Entity('armchairRed')
+// engine.addEntity(armchairRed)
+// armchairRed.setParent(_scene)
+// const transform10 = new Transform({
+//   position: new Vector3(5.5, 0, 18),
+//   rotation: new Quaternion(-0.013521425426006317, 0.7716787457466125, 0.013521332293748856, 0.6357250213623047),
+//   scale: new Vector3(1, 1, 1)
+// })
+// armchairRed.addComponentOrReplace(transform10)
+// const gltfShape6 = new GLTFShape("dc2d6a5f-0dc7-4300-8015-46993a3c425d/RedArmchair.glb")
+// gltfShape6.withCollisions = true
+// gltfShape6.isPointerBlocker = true
+// gltfShape6.visible = true
+// armchairRed.addComponentOrReplace(gltfShape6)
 
-const armchairRed2 = new Entity('armchairRed2')
-engine.addEntity(armchairRed2)
-armchairRed2.setParent(_scene)
-const transform11 = new Transform({
-  position: new Vector3(5.5, 0, 14.5),
-  rotation: new Quaternion(-4.470348002882929e-8, -0.7071068286895752, 1.639127589214695e-7, -0.7071067690849304),
-  scale: new Vector3(1, 1, 1)
-})
-armchairRed2.addComponentOrReplace(transform11)
-armchairRed2.addComponentOrReplace(gltfShape6)
+// const armchairRed2 = new Entity('armchairRed2')
+// engine.addEntity(armchairRed2)
+// armchairRed2.setParent(_scene)
+// const transform11 = new Transform({
+//   position: new Vector3(5.5, 0, 14.5),
+//   rotation: new Quaternion(-4.470348002882929e-8, -0.7071068286895752, 1.639127589214695e-7, -0.7071067690849304),
+//   scale: new Vector3(1, 1, 1)
+// })
+// armchairRed2.addComponentOrReplace(transform11)
+// armchairRed2.addComponentOrReplace(gltfShape6)
 
 const tableWithChairs = new Entity('tableWithChairs')
 engine.addEntity(tableWithChairs)
@@ -110,7 +110,7 @@ const transform12 = new Transform({
   scale: new Vector3(1, 1, 1)
 })
 tableWithChairs.addComponentOrReplace(transform12)
-const gltfShape7 = new GLTFShape("f7de372b-3ac8-4f56-a822-640326132014/Table_with_Chairs.glb")
+const gltfShape7 = new GLTFShape("models/Table_with_Chairs.glb")
 gltfShape7.withCollisions = true
 gltfShape7.isPointerBlocker = true
 gltfShape7.visible = true
@@ -125,7 +125,7 @@ const transform13 = new Transform({
   scale: new Vector3(1, 1, 1)
 })
 coffeeMachine.addComponentOrReplace(transform13)
-const gltfShape8 = new GLTFShape("bf7da256-1314-40dc-a5b4-19c8dc5196e4/Coffee_Machine.glb")
+const gltfShape8 = new GLTFShape("models/Coffee_Machine.glb")
 gltfShape8.withCollisions = true
 gltfShape8.isPointerBlocker = true
 gltfShape8.visible = true
@@ -148,7 +148,7 @@ const transform15 = new Transform({
   scale: new Vector3(1, 1, 1)
 })
 table.addComponentOrReplace(transform15)
-const gltfShape9 = new GLTFShape("619c08da-51a4-49db-9ad5-49e5e88b344b/Table 2.glb")
+const gltfShape9 = new GLTFShape("models/Table 2.glb")
 gltfShape9.withCollisions = true
 gltfShape9.isPointerBlocker = true
 gltfShape9.visible = true
@@ -163,7 +163,7 @@ const transform16 = new Transform({
   scale: new Vector3(1, 1, 1)
 })
 chairHighBack.addComponentOrReplace(transform16)
-const gltfShape10 = new GLTFShape("9cc4b4a2-46b6-4b49-aef9-da3959de0ac8/HighBackChair.glb")
+const gltfShape10 = new GLTFShape("models/HighBackChair.glb")
 gltfShape10.withCollisions = true
 gltfShape10.isPointerBlocker = true
 gltfShape10.visible = true
@@ -229,7 +229,7 @@ const transform22 = new Transform({
   rotation: new Quaternion(0, 0, 0, 1),
   scale: new Vector3(0.6, 0.6, 0.6)
 })
-const gltfShape11 = new GLTFShape("44bcc403-ae66-42db-ba5d-bd561040f607/Books.glb")
+const gltfShape11 = new GLTFShape("models/Books.glb")
 const books = new Books(
   gltfShape11,
   transform22
@@ -251,7 +251,7 @@ const transform23 = new Transform({
   scale: new Vector3(16, 1, 1)
 })
 concreteWall.addComponentOrReplace(transform23)
-const gltfShape12 = new GLTFShape("53aa3052-ccd7-4a05-be3f-f840acbff99e/Concrete_Wall.glb")
+const gltfShape12 = new GLTFShape("models/Concrete_Wall.glb")
 gltfShape12.withCollisions = true
 gltfShape12.isPointerBlocker = true
 gltfShape12.visible = true
@@ -299,7 +299,7 @@ const transform29 = new Transform({
   scale: new Vector3(2.15,1.8,2)
 })
 door3.addComponentOrReplace(transform29)
-const gltfShape13 = new GLTFShape("ee544d83-0a1e-4025-9a78-05cd9a605c16/Door 10.glb")
+const gltfShape13 = new GLTFShape("models/Door 10.glb")
 door3.addComponentOrReplace(gltfShape13)
 //Define start and end directions
 let StartRot = Quaternion.Euler(0, 90, 0)
